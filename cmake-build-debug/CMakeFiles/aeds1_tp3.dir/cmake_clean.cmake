7d6019e44a875b200fc059f11ca7788f951d186c
Lazaro <lazarobodevan@hotmail.com>
2019-11-21 22:18:26 -0300

    iniciando

---
+++ cmake-build-debug/CMakeFiles/aeds1_tp3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aeds1_tp3.dir/main.c.o"
+  "aeds1_tp3.pdb"
+  "aeds1_tp3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aeds1_tp3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/aeds1_tp3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for aeds1_tp3.
+# This may be replaced when dependencies are built.

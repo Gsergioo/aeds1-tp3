53bc9c5bba11e4b0f9e123e781c77189c4191c82
Guilherme Sérgio <53011345+Gsergioo@users.noreply.github.com>
2019-11-23 09:54:55 -0300

    Mudando apenas algumas coisas

--- Palavra.c
+++ Palavra.c
@@ -7,20 +7,24 @@
 #include <stdio.h>
 
 //Operacoes por Arranjo
-void inicializaPArr(TListaPArr *lista){
-    lista->primeiro = 0;
-    lista->ultimo = lista->primeiro;
-    lista->palavra->letra = (char*) malloc(45* sizeof(char));
+void inicializaPArr(TListaPArr *palavra){
+    palavra->primeiro = 0;
+    palavra->ultimo = palavra->primeiro;
+    palavra->letra = (char*) malloc(45* sizeof(char));
 }
-void insereLetraArr(char letra, TListaPArr *lista, int i){
-    lista->palavra->letra[i] = letra;
-    lista->ultimo++;
-    lista->palavra->primLetra = lista->palavra->letra[0];
+void insereLetraArr(Tletra letra, TListaPArr *lista){
+    if(lista->ultimo != 45){
+        lista->letra[lista->ultimo] = letra;
+        lista->ultimo++;
+    }
+    if(lista->ultimo - 1 == 0){
+        lista->primeiraletra = letra.letra;
+    }
 }
 int removeLetraArr(TListaPArr *lista, int pos){
-    if(pos <= lista->ultimo) {
+    if(pos <= lista->ultimo && pos >= lista->primeiro) {
         for (int i = pos + 1; i < lista->ultimo; i++) {
-            lista->palavra->letra[i - 1] = lista->palavra->letra[i];
+            lista->letra[i - 1] = lista->letra[i];
         }
         lista->ultimo--;
         return 1;
@@ -28,9 +32,11 @@ int removeLetraArr(TListaPArr *lista, int pos){
         return 0;
 }
 void imprimePalavraArr(TListaPArr *lista){
-        printf("%s",lista->palavra->letra);
-        printf(" ");
+    for (int i = 0; i < lista->ultimo; i++)
+        printf("%c", lista->letra[i].letra);
+    printf(" ");
 }
+
 int tamanhoPalavraArr(TListaPArr *lista){
     return lista->ultimo;
 }
@@ -41,23 +47,24 @@ int tamanhoPalavraArr(TListaPArr *lista){
 //Operacoes por Lista Encadeada
 
 
-void inicializaPLe(TCelulaP *celula, TListaPLe *lista){
+void inicializaPLe(TListaPLe *lista){
     lista->pPrimeiro = (TCelulaP*) malloc(sizeof(TCelulaP));
     lista->pUltimo = lista->pPrimeiro;
     lista->pUltimo->pProx = NULL;
+    lista->tam = 0;
 }
-void insereLetraLe(TCelulaP *celula, TListaPLe *lista, char letra){
+void insereLetraLe(TListaPLe *lista, Tletra letra){
     TCelulaP *aux;
     aux = (TCelulaP*) malloc(sizeof(TCelulaP));
     aux->pProx = NULL;
     lista->pUltimo->pProx = aux;
     lista->pUltimo = lista->pUltimo->pProx; //faz a ligação entre celulas
-    *lista->pUltimo->palavra.letra = letra; //insere a letra
-    lista->pPrimeiro->pProx->palavra.primLetra = *lista->pPrimeiro->pProx->palavra.letra; //armazena a primeira letra
+    lista->pUltimo->letra = letra; //insere a letra
+    lista->primeiraletra = lista->pPrimeiro->pProx->letra.letra ; //armazena a primeira letra
     lista->tam++;
 
 }
-void removeLetraLe(TPalavra *palavra, TCelulaP *celula){
+void removeLetraLe(Tletra *letra, TCelulaP *celula){
     //nao sei se e pra remover uma letra inserida e procurar pela primeira ocorrencia
     //nao sei se e pra remover uma posicao especifica
     //nao sei se e pra remover a ultima/primeira letra
@@ -66,7 +73,7 @@ void imprimePalavraLe(TListaPLe *lista){
     TCelulaP *aux;
     aux = lista->pPrimeiro->pProx;
     while(aux->pProx != NULL){
-        printf("%c", *aux->palavra.letra);
+        printf("%c", aux->letra.letra);
         aux = aux->pProx;
     }
     printf(" ");

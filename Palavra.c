7216b1ee17f8911a07dedaf8e4c22cc9f75919da
Guilherme Sérgio <53011345+Gsergioo@users.noreply.github.com>
2019-11-28 08:17:46 -0300

    Adicionado QuickSort

--- Palavra.c
+++ Palavra.c
@@ -3,8 +3,7 @@
 //
 
 #include "Palavra.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 
 //Operacoes por Arranjo
 void inicializaPArr(TListaLArr *palavra){
@@ -12,8 +11,7 @@ void inicializaPArr(TListaLArr *palavra){
     palavra->ultimo = palavra->primeiro;
     palavra->letra = (Tletra*) malloc(45* sizeof(char));
 }
-
-void insereLetraArr(TListaLArr *lista){ //Cria palavras aleatorias
+void insereLetraArr(TListaLArr *lista){
     int tam = rand()%44;
     for(int i = 0; i < tam; i++){
         lista->letra[i].letra = 'a' + rand()%26;
@@ -31,6 +29,7 @@ int removeLetraArr(TListaLArr *lista, int pos){
     } else
         return 0;
 }
+
 void imprimePalavraArr(TListaLArr *lista){
     for (int i = 0; i < lista->ultimo; i++)
         printf("%c", lista->letra[i].letra);
@@ -51,23 +50,24 @@ void inicializaPLe(TListaLLe *lista){
     lista->pPrimeiro = (TCelulaL*) malloc(sizeof(TCelulaL));
     lista->pUltimo = lista->pPrimeiro;
     lista->pUltimo->pProx = NULL;
+    lista->pPrimeiro->pAnte = NULL;
     lista->tam = 0;
 }
-
-void insereLetraLe(TListaLLe *lista){ //Cria palavras aleatorias
+void insereLetraLe(TListaLLe *lista){
     TCelulaL *aux;
-    int tam = rand()%45;
+    int tam = rand()%10;
     for(int i = 0; i < tam; i++){
         aux = (TCelulaL*) malloc(sizeof(TCelulaL));
         aux->pProx = NULL;
+        aux->pAnte = lista->pUltimo;
         lista->pUltimo->pProx = aux;
         lista->pUltimo = lista->pUltimo->pProx; //faz a ligação entre celulas
         lista->pUltimo->letra.letra = 'a' + rand()%26; //insere a letra
     }
-    lista->primeiraletra = lista->pPrimeiro->pProx->letra.letra ; //armazena a primeira letra
+    if(lista->pPrimeiro == lista->pUltimo)   lista->primeiraletra = lista->pPrimeiro->pProx->letra.letra ; //armazena a primeira letra
     lista->tam++;
+    lista->pUltimo->indice = lista->tam;
 }
-
 void removeLetraLe(Tletra *letra, TCelulaL *celula){
     //nao sei se e pra remover uma letra inserida e procurar pela primeira ocorrencia
     //nao sei se e pra remover uma posicao especifica
@@ -84,4 +84,51 @@ void imprimePalavraLe(TListaLLe *lista){
 }
 int tamanhoPalavraLe(TListaLLe *lista){
     return lista->tam;
+}
+
+void ordena(TCelulaL* esq, TCelulaL* dir, TListaLLe *lista){
+    TCelulaL* i;
+    TCelulaL* j;
+    particao(esq, dir, &i, &j, lista);
+
+    if(esq->indice < j->indice) ordena(esq, j, lista);
+    if(dir->indice > i->indice) ordena(i, dir, lista);
+}
+
+
+void quicksort(TListaLLe *lista){
+    ordena(lista->pPrimeiro->pProx, lista->pUltimo, lista);
+}
+
+void particao(TCelulaL* esq, TCelulaL* dir, TCelulaL** ii, TCelulaL** jj, TListaLLe* lista){
+    int pivo;
+    int cont1, cont2;
+    cont1 = esq->indice;
+    cont2 = dir->indice;
+    TCelulaL* aux = lista->pPrimeiro->pProx;
+    TCelulaL* i = esq;
+    TCelulaL* j = dir;
+    for(int k = 1; k < ((int)(i->indice + j->indice)/2); k++)
+        aux = aux->pProx;
+    pivo = aux->letra.letra;
+    do{
+        while(i->letra.letra < pivo) {i = i->pProx; cont1++;}
+        while (j->letra.letra > pivo){ j = j->pAnte; cont2--;}
+        if(cont1 <= cont2){
+            troca(i, j);
+            i = i->pProx;
+            cont1++;
+            cont2--;
+            j = j->pAnte;
+        }
+    }while(cont1 <= cont2);
+    *ii = i;
+    *jj = j;
+}
+
+void troca(TCelulaL* i, TCelulaL* j){
+    char aux;
+    aux = i->letra.letra;
+    i->letra.letra = j->letra.letra;
+    j->letra.letra = aux;
 }

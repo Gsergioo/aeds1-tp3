3189022d08f062d2deca833d15fd752798fa7549
Guilherme Sérgio <53011345+Gsergioo@users.noreply.github.com>
2019-11-28 22:58:39 -0300

    Update Palavra.c

--- Palavra.c
+++ Palavra.c
@@ -7,9 +7,9 @@
 
 //Operacoes por Arranjo
 void inicializaPArr(TListaLArr *palavra){
-    palavra->letra = (Tletra*) malloc(45 * sizeof(Tletra));
     palavra->primeiro = 0;
     palavra->ultimo = palavra->primeiro;
+    palavra->letra = (Tletra*) malloc(45* sizeof(char));
 }
 void insereLetraArr(TListaLArr *lista){
     int tam = rand()%44;
@@ -49,6 +49,7 @@ int tamanhoPalavraArr(TListaLArr *lista){
 void inicializaPLe(TListaLLe *lista){
     lista->pPrimeiro = (TCelulaL*) malloc(sizeof(TCelulaL));
     lista->pUltimo = lista->pPrimeiro;
+    lista->pPrimeiro->indice = -1;
     lista->pUltimo->pProx = NULL;
     lista->pPrimeiro->pAnte = NULL;
     lista->tam = 0;
@@ -56,27 +57,27 @@ void inicializaPLe(TListaLLe *lista){
 void insereLetraLe(TListaLLe *lista){
     TCelulaL *aux;
     int tam = rand()%10;
-    for(int i = 0; i < tam; i++){
+    for(int i = 0; i < 10; i++){
         aux = (TCelulaL*) malloc(sizeof(TCelulaL));
         aux->pProx = NULL;
         aux->pAnte = lista->pUltimo;
         lista->pUltimo->pProx = aux;
         lista->pUltimo = lista->pUltimo->pProx; //faz a ligação entre celulas
         lista->pUltimo->letra.letra = 'a' + rand()%26; //insere a letra
+        if(lista->pPrimeiro->pProx == lista->pUltimo)   lista->primeiraletra = lista->pPrimeiro->pProx->letra.letra ; //armazena a primeira letra
+        lista->tam++;
+        lista->pUltimo->indice = lista->tam;
     }
-    if(lista->pPrimeiro == lista->pUltimo)   lista->primeiraletra = lista->pPrimeiro->pProx->letra.letra ; //armazena a primeira letra
-    lista->tam++;
-    lista->pUltimo->indice = lista->tam;
 }
 void removeLetraLe(Tletra *letra, TCelulaL *celula){
-    //nao sei se e pra remover uma letra inserida e procurar pela primeira ocorrencia
+    //nao sei se e pra remover uma letra inserida e p+rocurar pela primeira ocorrencia
     //nao sei se e pra remover uma posicao especifica
     //nao sei se e pra remover a ultima/primeira letra
 }
 void imprimePalavraLe(TListaLLe *lista){
     TCelulaL *aux;
     aux = lista->pPrimeiro->pProx;
-    while(aux->pProx != NULL){
+    while(aux != NULL){
         printf("%c", aux->letra.letra);
         aux = aux->pProx;
     }
@@ -90,6 +91,8 @@ void ordena(TCelulaL* esq, TCelulaL* dir, TListaLLe *lista){
     TCelulaL* i;
     TCelulaL* j;
     particao(esq, dir, &i, &j, lista);
+    printf("indice i: %c ", i->letra);
+    printf("indice j : %c ", j->letra);
 
     if(esq->indice < j->indice) ordena(esq, j, lista);
     if(dir->indice > i->indice) ordena(i, dir, lista);
@@ -101,16 +104,18 @@ void quicksort(TListaLLe *lista){
 }
 
 void particao(TCelulaL* esq, TCelulaL* dir, TCelulaL** ii, TCelulaL** jj, TListaLLe* lista){
-    int pivo;
+    char pivo;
     int cont1, cont2;
     cont1 = esq->indice;
     cont2 = dir->indice;
     TCelulaL* aux = lista->pPrimeiro->pProx;
     TCelulaL* i = esq;
     TCelulaL* j = dir;
-    for(int k = 1; k < ((int)(i->indice + j->indice)/2); k++)
+    int tam = (cont1 + cont2)/2;
+    for(int k = 1; k < (tam); k++)
         aux = aux->pProx;
     pivo = aux->letra.letra;
+    printf("pivo: %c", pivo);
     do{
         while(i->letra.letra < pivo) {i = i->pProx; cont1++;}
         while (j->letra.letra > pivo){ j = j->pAnte; cont2--;}

57d2cb69e01d7ad517789c0ba13a181a6a8061dd
Guilherme Sérgio <53011345+Gsergioo@users.noreply.github.com>
2019-11-30 19:33:29 -0300

    Adição select e quick array e quick Le

--- Biblioteca.h
+++ Biblioteca.h
@@ -36,6 +36,10 @@ void insereTextoArr(TListaTArr *lista, int qtdPalavras, int qtdTexto);
 void removeTextoArr(TListaTArr *lista);
 int tamanhoBibliotecaArr(TListaTArr *lista);
 void imprimeBibliotecaArr(TListaTArr* lista);
+void selectionSortBibArr(TListaTArr lista);
+void quicksortBibArr(TListaTArr lista);
+void ordenaBibArr(int esq, int dir, TListaTArr *lista);
+void particaoBibArr(int esq, int dir, int* i, int* j, TListaTArr* lista);
 
 //Operacoes por lista encadeada
 void inicializaBiblioLe(TListaTLe *lista);
@@ -44,3 +48,7 @@ void removeTextoLe(TListaTLe *lista);
 int tamanhoBibliotecaLe(TListaTLe *lista);
 void imprimeBibliotecaLe(TListaTLe *biblioteca);
 void selectionSortBibLe(TListaTLe lista);
+void quicksortBibLe(TListaTLe lista);
+void ordenaBibLe(TCelulaT* esq, TCelulaT* dir, TListaTLe *texto, double* comp);
+void particaoBibLe(TCelulaT* esq, TCelulaT* dir, TCelulaT** ii, TCelulaT** jj, TListaTLe* texto, double* comp);
+void trocaBibLe(TCelulaT* i, TCelulaT* j);

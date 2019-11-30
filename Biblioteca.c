8727a0b0463ef32ea85d6392499f29b143b7df9d
Guilherme Sérgio <53011345+Gsergioo@users.noreply.github.com>
2019-11-30 01:19:17 -0300

    atualização operações principais e selectio sort

--- Biblioteca.c
+++ Biblioteca.c
@@ -3,18 +3,21 @@
 //
 
 #include "Biblioteca.h"
-#include <stdlib.h>
-#define YEL   "\x1B[33m"
-#define RESET "\x1B[0m"
+
 //Operacoes por arranjo
 void inicializaBiblioArr(TListaTArr *lista){
-    lista->biblioteca = (TListaPArr*) malloc(lista->biblioteca->ultimo * sizeof(TListaPArr));
+    lista->biblioteca = (TListaPArr*) malloc(10* sizeof(TListaPArr));
     lista->primeiro = 0;
     lista->ultimo = lista->primeiro;
 }
 void insereTextoArr(TListaTArr *lista){
-//    lista->biblioteca[lista->ultimo] = *texto;
-//    lista->ultimo++;
+    TListaPArr texto;
+    for(int i = 0; i < 10; i++){
+        inicializaTextoArr(&texto);
+        inserePalavraArr(&texto, 10);
+        lista->biblioteca[lista->ultimo] = texto;
+        lista->ultimo++;
+    }
     printf("Inserida!");
 }
 void removeTextoArr(TListaTArr *lista){
@@ -29,6 +32,13 @@ int tamanhoBibliotecaArr(TListaTArr *lista){
     return lista->ultimo;
 }
 
+void imprimeBibliotecaArr(TListaTArr *lista){
+    for (int i = 0; i < lista->ultimo; i++){
+        imprimeTextoArr(&(lista->biblioteca[i]));
+        printf("Texto n \n");
+    }
+}
+
 //Operacoes por lista encadeada
 void inicializaBiblioLe(TListaTLe *lista){
     lista->pPrimeiro = (TCelulaT*) malloc(sizeof(TCelulaT));
@@ -40,14 +50,16 @@ void inicializaBiblioLe(TListaTLe *lista){
 }
 void insereTextoLe(TListaTLe *lista, int tam, int tam2){
     TCelulaT *aux;
+    int al = rand()%26;
     for(int i = 0; i < tam; i++) {
+        al = 1 + rand()%26;
         aux = (TCelulaT *) malloc(sizeof(TCelulaT));
         aux->pProx = NULL;
         aux->pAnte = lista->pUltimo;
         lista->pUltimo->pProx = aux;
         lista->pUltimo = aux;
         inicializaTextoLe(&(aux->texto));
-        inserePalavraLe(&(aux->texto), tam2);
+        inserePalavraLe(&(aux->texto), al);
         lista->tam++;
         aux->indice = lista->tam;
     }
@@ -63,6 +75,40 @@ void removeTextoLe(TListaTLe *lista){
         printf(YEL"----> Não é possível remover!" RESET);
     }
 }
+void imprimeBibliotecaLe(TListaTLe *biblioteca){
+    TCelulaT* aux = biblioteca->pPrimeiro->pProx;
+    while (aux != NULL){
+        imprimeTextoLe(&(aux->texto));
+        printf(" Texto %d\n", aux->texto.tam);
+        aux = aux->pProx;
+    }
+
+}
+
 int tamanhoBibliotecaLe(TListaTLe *lista){
     return lista->tam;
+}
+
+void selectionSortBibLe(TListaTLe lista){
+    TCelulaT *aux, *aux2;
+    TCelulaT* min = lista.pPrimeiro;
+    TListaPLe texto, textoaux;
+
+    for (int i = 0; i < lista.tam - 1; i++){
+        min = min->pProx;
+        texto = min->texto;
+        aux = min->pProx;
+        aux2 = min;
+        for (int j = i + 1; j < lista.tam; j++){
+            if (texto.tam > aux->texto.tam){
+                texto = aux->texto;
+                aux2 = aux;
+            }
+            aux = aux->pProx;
+        }
+        textoaux = min->texto;
+        min->texto = texto;
+        aux2->texto = textoaux;
+    }
+    imprimeBibliotecaLe(&lista);
 }
